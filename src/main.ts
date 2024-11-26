4e885afa954e5291d8ddffcf9083171cd04fe6f5
Erick Christian <erickchristian48@gmail.com>
2024-11-27 00:18:00 +0700

    feat: add !start command

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,23 @@ client.on(Events.MessageCreate, async (message) => {
   if (message.content.startsWith('!hello')) {
     await message.reply('Hello! I am your Discord bot!')
   }
+
+  // Create a thread command
+  if (message.content.startsWith('!start')) {
+    try {
+      const threadName = message.content.split(' ').slice(1).join(' ') || 'New Discussion'
+      const thread = await message.startThread({
+        autoArchiveDuration: 60,
+        name: threadName,
+        reason: 'Created a new thread via command',
+      })
+      await thread.send('Thread created! Let\'s start the discussion.')
+    }
+    catch (error) {
+      console.error('Error creating thread:', error)
+      await message.reply('Sorry, I couldn\'t create the thread. Make sure I have the necessary permissions!')
+    }
+  }
 })
 
 // Log in to Discord with your client's token
